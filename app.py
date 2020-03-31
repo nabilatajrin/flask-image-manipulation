4cb5b083e954aec654d05d6bacd3a1781b56bba2
nabila <nabilatajrin@gmail.com>
2020-03-31 17:58:23 +0600

    update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import PIL.Image
 import flask
 
 from flask import Flask, request, send_file
-
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -12,7 +12,7 @@ def home():
     return "Hello World Rain!"
 
 
-# upload selected image and forward to processing page
+# download selected image and forward to processing page
 @app.route("/download", methods=['GET', 'POST'])
 def download():
     if request.args.get('type') == '1':
@@ -22,15 +22,24 @@ def download():
     return send_file(filename, mimetype='b/png')
 
 
+# upload selected image and forward to processing page
+@app.route("/upload", methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded'
+
+
 @app.route("/img", methods=['GET', 'POST'])
 def img():
     if request.args.get('type') == '1':
-        filename = 'b.png'
+        filename = request.files['file']
         image = PIL.Image.open(filename)
         width, height = image.size
         x = width + height
     else:
-        filename = 'b.png'
+        filename = request.files['file']
         image = PIL.Image.open(filename)
         width, height = image.size
         response = "<h3>Width: </h3>" + str(width) + "<h3>Height: </h3>" + str(height)
