e20b3b730a3badebb5f78b421cd1e9db966b95a4
nabila <nabilatajrin@gmail.com>
2020-04-16 12:01:39 +0600

    12:04PM 16APR2020 updated readme

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-from typing import io
 import PIL.Image
-import flask
-import numpy
-import os
 from flask import Flask, request, send_file
-from imageio import imread
 from werkzeug.utils import secure_filename
-import cv2
 
 # instantiate/ create the application
 app = Flask(__name__)
@@ -21,56 +15,49 @@ def home():
     return "Welcome to home page!"
 
 # check whether an image is new or old based on logic if the image is color or grayscale.
+
+# there could be different methods on it.
+# method includes the type of requests it is getting sends over
+# ['GET', 'POST'] means it will accept get request and post request.
 @app.route("/img", methods=['GET', 'POST'])
 def img():
     filename = request.files['file']
     image = PIL.Image.open(filename)
     width, height = image.size
 
-    if request.args.get('type') == '1':
-        response = width + height
-    else:
-        filename2 = secure_filename(filename.filename)
-        # img = cv2.imread("01.jpeg", 1)
-        # rgb = str(img.shape)
-        # x=os.getcwd()
+    filename2 = secure_filename(filename.filename)
 
-        for i in range(width):
-            for j in range(height):
-                r, g, b = image.getpixel((i, j))
-                # rgb = str(image.shape)
-                if r != g != b:
-                    color = 'Color image'
-                    year = '2019'
-                else:
-                    color = 'Grayscale image'
-                    year = '1990'
+    for i in range(width):
+        for j in range(height):
+            r, g, b = image.getpixel((i, j))
+            # rgb = str(image.shape)
+            if r != g != b:
+                color = 'Color image'
+                year = '2019'
+            else:
+                color = 'Grayscale image'
+                year = '1990'
 
-        # img = cv2.imread('01.jpeg', 1) #1 means keep the img as it is, 0 converts to grayscale
-        # if (len(img.shape) < 3):
-        #     x = 'gray'
-        #     y = str(img.shape)
-        # elif len(img.shape) == 3:
-        #     x = 'Color(RGB)'
-        #     y = str(img.shape)
-        # else:
-        #     x = 'others'
-        #     y = str(img.shape)
+    # img = cv2.imread('01.jpeg', 1) #1 means keep the img as it is, 0 converts to grayscale
+    # if (len(img.shape) < 3):
+    #     x = 'gray'
+    #     y = str(img.shape)
+    # elif len(img.shape) == 3:
+    #     x = 'Color(RGB)'
+    #     y = str(img.shape)
+    # else:
+    #     x = 'others'
+    #     y = str(img.shape)
 
-        response = "<h3>File name: </h3>" + filename2 + \
-                   "<h3>Height & Width: </h3>" + str(height) + "px,  " + str(width) + "px" + \
-                   "<h3>Color: </h3>" + color + \
-                    "<h3>Year: </h3>" + year
+    response = "<h3>File name: </h3>" + filename2 + \
+               "<h3>Height & Width: </h3>" + str(height) + "px,  " + str(width) + "px" + \
+               "<h3>Color: </h3>" + color + \
+                "<h3>Year: </h3>" + year
 
     return response
 
 
-
-
 # download selected image and forward to processing page
-# there could be different methods on it.
-# method includes the type of requests it is getting sends over
-# ['GET', 'POST'] means it will accept get request and post request.
 @app.route("/download", methods=['GET', 'POST'])
 def download():
     if request.args.get('type') == '1':
