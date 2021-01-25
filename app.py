fa6a290a6a0a92cd5dece3133d1cc7e0291474f5
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-01-25 11:03:00 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from werkzeug.utils import secure_filename
 
 # instantiate/ create the application
 app = Flask(__name__)
-
-
 # route, when someone calls the route the code associated with it will get run
 # an app can handle multiple routs
 # this is also an endpoint. an app can have multiple endpoints.
@@ -13,9 +11,7 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def home():
     return "Welcome to home page!"
-
 # check whether an image is new or old based on logic if the image is color or grayscale.
-
 # there could be different methods on it.
 # method includes the type of requests it is getting sends over
 # ['GET', 'POST'] means it will accept get request and post request.
@@ -25,9 +21,7 @@ def img():
     username = request.form['username']
     image = PIL.Image.open(filename)
     width, height = image.size
-
     filename2 = filename.filename
-
     for i in range(width):
         for j in range(height):
             r, g, b = image.getpixel((i, j))
@@ -38,7 +32,6 @@ def img():
             else:
                 color = 'Grayscale image'
                 year = '1990'
-
     # img = cv2.imread('01.jpeg', 1) #1 means keep the img as it is, 0 converts to grayscale
     # if (len(img.shape) < 3):
     #     x = 'gray'
@@ -49,16 +42,12 @@ def img():
     # else:
     #     x = 'others'
     #     y = str(img.shape)
-
     response = "<h3>File name: </h3>" + filename2 + \
                "<h3>Height & Width: </h3>" + str(height) + "px,  " + str(width) + "px" + \
                "<h3>Color: </h3>" + color + \
                 "<h3>Year: </h3>" + year + \
                 "<h3>Username: </h3>" + username
-
     return response
-
-
 # if I call the file directly from the command line I want to execute what follows here
 if __name__ == '__main__':
     # run the app
